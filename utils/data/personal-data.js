11f8fa2a9200da07192adb12abaebcdc764fb178
Sheikh Zubair <39708100+shakesbeer2049@users.noreply.github.com>
2024-10-09 10:05:32 +0530

    updated resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,13 +1,15 @@
 export const personalData = {
   name: "Sheikh Zubair",
-  profile: '/profile.png',
+  profile: "/profile.png",
   designation: "Software Developer",
-  description: "My name is Sheikh Zubair. I am a enthusiastic Software Engineer. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'shakesbeer2049@gmail.com',
-  phone: '+919858493108',
-  address: 'Srinagar, J&K, India',
-  github: 'https://github.com/shakesbeer2049',
-  linkedIn: 'https://www.linkedin.com/in/shakesbeer2049/',
+  description:
+    "My name is Sheikh Zubair. I am a enthusiastic Software Engineer. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "shakesbeer2049@gmail.com",
+  phone: "+919858493108",
+  address: "Srinagar, J&K, India",
+  github: "https://github.com/shakesbeer2049",
+  linkedIn: "https://www.linkedin.com/in/shakesbeer2049/",
   leetcode: "https://leetcode.com/u/user8017Qp/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-}
+  resume:
+    "https://docs.google.com/document/d/e/2PACX-1vSLtB25Jzuxo4_obosA6RZd1WLf2VtkhkCRfuedqNq1FB_meIb-EE-FkVvXkvA6i1ADCWWkQpWD8vz6/pub",
+};
